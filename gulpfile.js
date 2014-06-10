648200becf7611eadf9e8084927e2116db5fdc6a
Sahat Yalkabov <sakhat@gmail.com>
2014-06-10 00:53:10 -0400

    Added jQuery Added gulpfile.js for generating binary file Removed kiosk mode property

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var downloadatomshell = require('gulp-download-atom-shell');
+
+gulp.task('downloadatomshell', function(cb){
+  downloadatomshell({
+    version: '0.12.5',
+    outputDir: 'binaries'
+  }, cb);
+});
+
+gulp.task('default', ['downloadatomshell']);
