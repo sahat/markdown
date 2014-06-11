6143b21f61bd18ea1f2a11f1a1bd53380b41f899
Sahat Yalkabov <sakhat@gmail.com>
2014-06-11 09:31:41 -0400

    Swapped code mirror with Pen editor

--- js/app.js
+++ js/app.js
@@ -1,7 +1,70 @@
-var editor = document.getElementById('editor');
+var editor = new Pen('#editor');
 
-var myCodeMirror = CodeMirror(editor, {
-  mode: 'markdown',
-  theme: 'default'
-});
+// Load native UI library
+var gui = require('nw.gui');
 
+// Create an empty menu
+var menu = new gui.Menu();
+
+// Add some items with label
+menu.append(new gui.MenuItem({ label: 'Item A' }));
+menu.append(new gui.MenuItem({ label: 'Item B' }));
+menu.append(new gui.MenuItem({ type: 'separator' }));
+menu.append(new gui.MenuItem({ label: 'Item C' }));
+
+// Remove one item
+menu.removeAt(1);
+
+// Iterate menu's items
+for (var i = 0; i < menu.items.length; ++i) {
+  console.log(menu.items[i]);
+}
+
+// Add a item and bind a callback to item
+menu.append(new gui.MenuItem({
+  label: 'Click Me',
+  click: function() {
+    // Create element in html body
+    var element = document.createElement('div');
+    element.appendChild(document.createTextNode('Clicked OK'));
+    document.body.appendChild(element);
+  }
+}));
+
+// Popup as context menu
+document.body.addEventListener('contextmenu', function(ev) {
+  ev.preventDefault();
+  // Popup at place you click
+  menu.popup(ev.x, ev.y);
+  return false;
+}, false);
+
+// Get the current window
+var win = gui.Window.get();
+
+// Create a menubar for window menu
+var menubar = new gui.Menu({ type: 'menubar' });
+
+// Create a menuitem
+var sub1 = new gui.Menu();
+
+
+sub1.append(new gui.MenuItem({
+  label: 'Test1',
+  click: function() {
+    var element = document.createElement('div');
+    element.appendChild(document.createTextNode('Test 1'));
+    document.body.appendChild(element);
+  }
+}));
+
+// You can have submenu!
+menubar.append(new gui.MenuItem({ label: 'Sub1', submenu: sub1}));
+
+//assign the menubar to window menu
+win.menu = menubar;
+
+// add a click event to an existing menuItem
+menu.items[0].click = function() {
+  console.log("CLICK");
+};
