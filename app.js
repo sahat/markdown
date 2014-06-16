d59dd2e1482dd1b9b5450fa2a48c544385ff7eba
Sahat Yalkabov <sakhat@gmail.com>
2014-06-16 12:49:41 -0400

    Emit edit mode when post is selected

--- app.js
+++ app.js
@@ -6,31 +6,29 @@ var http = require('http');
 var md = require('html-md');
 var _ = require('lodash');
 var gui = require('nw.gui');
+//
+//// Open a new window.
+//var win = gui.Window.get();
+//
+//// Release the 'win' object here after the new window is closed.
+//win.on('closed', function() {
+//  win = null;
+//});
+//
+//// Listen to main window's close event
+//gui.Window.get().on('close', function() {
+//  // Hide the window to give user the feeling of closing immediately
+//  this.hide();
+//
+//  // If the new window is still open then close it.
+//  if (win != null)
+//    win.close(true);
+//
+//  // After closing the new window, close the main window.
+//  this.close(true);
+//});
 
-// Open a new window.
-var win = gui.Window.get();
 
-// Release the 'win' object here after the new window is closed.
-win.on('closed', function() {
-  win = null;
-});
-
-// Listen to main window's close event
-gui.Window.get().on('close', function() {
-  // Hide the window to give user the feeling of closing immediately
-  this.hide();
-
-  // If the new window is still open then close it.
-  if (win != null)
-    win.close(true);
-
-  // After closing the new window, close the main window.
-  this.close(true);
-});
-
-function getUrl() {
-  console.log(document.getElementsByTagName('iframe')[0].contentWindow.location.pathname);
-}
 
 var flow = new EventEmitter();
 
@@ -38,7 +36,7 @@ document.getElementById('openBlog').addEventListener('click', function() {
   var chooser = document.getElementById('fileDialog');
   chooser.addEventListener('change', function() {
     var blogDir = this.value;
-    flow.emit('start-jekyll', blogDir);
+    flow.emit('startJekyll', blogDir);
     flow.emit('getMarkdownFiles', blogDir);
   });
   chooser.click();
@@ -48,7 +46,18 @@ flow.on('loading', function() {
   document.getElementById('home').innerHTML = '<h2 class="text-center">Loading...</h2>';
 });
 
-flow.on('start-http-server', function(source) {
+flow.on('editMode', function() {
+  console.log('EDIT MODE IS ON');
+  document.getElementById('savePost').innerHTML = '<iframe onload="getUrl()" src="' + url + '" width="100%" height="100%" frameborder="0"></iframe>';
+  document.getElementById('publishPost').innerHTML = '<iframe onload="getUrl()" src="' + url + '" width="100%" height="100%" frameborder="0"></iframe>';
+});
+
+flow.on('startHttpServer', function(source) {
+
+  var getUrl = function() {
+    console.log(document.getElementsByTagName('iframe')[0].contentWindow.location.pathname);
+    flow.emit('editMode');
+  };
 
   var server = spawn('node', ['node_modules/http-server/bin/http-server', source + '/_site']);
 
@@ -57,6 +66,7 @@ flow.on('start-http-server', function(source) {
     var url = 'http://localhost:8080';
     document.querySelector('body').classList.remove('cover');
     document.getElementById('main').innerHTML = '<iframe onload="getUrl()" src="' + url + '" width="100%" height="100%" frameborder="0"></iframe>';
+
     flow.emit('show-top-bar');
   });
 
@@ -69,15 +79,15 @@ flow.on('start-http-server', function(source) {
   });
 });
 
-flow.on('start-jekyll', function(source) {
+flow.on('startJekyll', function(source) {
   flow.emit('loading');
   var child = exec('jekyll build -s ' + source, function(err, stdout, stderr) {
     console.log(stdout);
-    flow.emit('start-http-server', source);
+    flow.emit('startHttpServer', source);
   });
 });
 
-flow.on('show-top-bar', function() {
+flow.on('showTopBar', function() {
   document.querySelector('.fixed').classList.remove('hidden');
 });
 
@@ -133,77 +143,3 @@ flow.on('getMarkdownFiles', function(path) {
 //});
 
 
-//http.get('http://localhost:4000', function(err, data) {
-//  $('#container').html(data);
-//});
-
-
-//
-//// Load native UI library
-//var gui = require('nw.gui');
-//
-//// Create an empty menu
-//var menu = new gui.Menu();
-//
-//// Add some items with label
-//menu.append(new gui.MenuItem({ label: 'Item A' }));
-//menu.append(new gui.MenuItem({ label: 'Item B' }));
-//menu.append(new gui.MenuItem({ type: 'separator' }));
-//menu.append(new gui.MenuItem({ label: 'Item C' }));
-//
-//// Remove one item
-//menu.removeAt(1);
-//
-//// Iterate menu's items
-//for (var i = 0; i < menu.items.length; ++i) {
-//  console.log(menu.items[i]);
-//}
-//
-//// Add a item and bind a callback to item
-//menu.append(new gui.MenuItem({
-//  label: 'Click Me',
-//  click: function() {
-//    // Create element in html body
-//    var element = document.createElement('div');
-//    element.appendChild(document.createTextNode('Clicked OK'));
-//    document.body.appendChild(element);
-//  }
-//}));
-//
-//// Popup as context menu
-//document.body.addEventListener('contextmenu', function(ev) {
-//  ev.preventDefault();
-//  // Popup at place you click
-//  menu.popup(ev.x, ev.y);
-//  return false;
-//}, false);
-//
-//// Get the current window
-//var win = gui.Window.get();
-//
-//// Create a menubar for window menu
-//var menubar = new gui.Menu({ type: 'menubar' });
-//
-//// Create a menuitem
-//var sub1 = new gui.Menu();
-//
-//
-//sub1.append(new gui.MenuItem({
-//  label: 'Test1',
-//  click: function() {
-//    var element = document.createElement('div');
-//    element.appendChild(document.createTextNode('Test 1'));
-//    document.body.appendChild(element);
-//  }
-//}));
-//
-//// You can have submenu!
-//menubar.append(new gui.MenuItem({ label: 'Sub1', submenu: sub1}));
-//
-////assign the menubar to window menu
-//win.menu = menubar;
-//
-//// add a click event to an existing menuItem
-//menu.items[0].click = function() {
-//  console.log("CLICK");
-//};
