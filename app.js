4c87dffb05d9ce3dcfae4b6f80f7d6b81e744de3
Sahat Yalkabov <sakhat@gmail.com>
2014-06-19 17:27:26 -0400

    Code cleanup

--- app.js
+++ app.js
@@ -8,129 +8,6 @@ var spawn = require('child_process').spawn;
 var Promise = require('bluebird');
 var gui = require('nw.gui');
 
-//var flow = new EventEmitter();
-//
-//document.getElementById('openBlog').addEventListener('click', function() {
-//  var chooser = document.getElementById('fileDialog');
-//  chooser.addEventListener('change', function() {
-//    var blogDir = this.value;
-//    flow.emit('startJekyll', blogDir);
-//    flow.emit('getMarkdownFiles', blogDir);
-//  });
-//  chooser.click();
-//});
-//
-//function getUrl() {
-//  var path = document.getElementsByTagName('iframe')[0].contentWindow.location.pathname;
-//  flow.emit('iframeUrl', path);
-//}
-//
-//flow.on('loading', function() {
-//  document.getElementById('home').innerHTML = '<h2 class="text-center">Loading...</h2>';
-//});
-//
-//flow.on('editMode', function() {
-//  console.log('EDIT MODE IS ON');
-//  document.getElementById('savePost').classList.remove('hidden');
-//  document.getElementById('publishPost').classList.remove('hidden');
-//});
-//
-//flow.on('startHttpServer', function(path) {
-//  var server = spawn('node', ['node_modules/http-server/bin/http-server', path + '/_site']);
-//
-//  server.stdout.once('data', function (data) {
-//    console.log('stdout: ' + data);
-//    var url = 'http://localhost:8080';
-//    document.querySelector('body').classList.remove('cover');
-//    document.getElementById('main').innerHTML = '<iframe onload="getUrl()" src="' + url + '" width="100%" height="100%" frameborder="0"></iframe>';
-//    flow.emit('showTopBar');
-//    flow.emit('getMarkdownFiles');
-//  });
-//
-//  server.stderr.on('data', function (data) {
-//    console.log('stderr: ' + data);
-//  });
-//
-//  server.on('close', function (code) {
-//    console.log('child process exited with code ' + code);
-//  });
-//});
-//
-//flow.on('startJekyll', function(source) {
-//  flow.emit('loading');
-//  var child = exec('jekyll build -s ' + source, function(err, stdout, stderr) {
-//    console.log(stdout);
-//    flow.emit('startHttpServer', source);
-//  });
-//});
-//
-//flow.on('showTopBar', function() {
-//  document.querySelector('.fixed').classList.remove('hidden');
-//});
-//
-//flow.on('getMarkdownFiles', function(path) {
-//  fs.readdir(path + '/_posts', function(err, files) {
-//
-//    if (!files) {
-//      alert('No posts found');
-//    }
-//
-//    var posts = _.filter(files, function(file) {
-//      var extension = file.split('.').pop();
-//      return extension === 'md' || extension === 'markdown'
-//    });
-//
-//    flow.emit('checkIfInEditMode', posts);
-//  });
-//});
-//
-//
-//flow.on('checkIfInEditMode', function(posts) {
-//  flow.on('iframeUrl', function(url) {
-//    console.log(posts);
-//    console.log(url);
-//
-//    var url = url.replace(/\//g, '');
-//    var posts = _.map(posts, function(post) { return post.split('-').slice(3).join('-').split('.').shift() });
-//  });
-//});
-
-//flow.on('readMarkdownFiles', function(path, posts) {
-//
-//  var renderer = new marked.Renderer();
-//
-//  renderer.image = function(href, title, text) {
-//    var out = '<img src="' + path + href + '" alt="' + text + '"';
-//    if (title) {
-//      out += ' title="' + title + '"';
-//    }
-//    out += this.options.xhtml ? '/>' : '>';
-//    return out;
-//  };
-//
-//  _.each(posts, function(post) {
-//
-//    var file = fs.readFileSync(path + '/_posts/' + post, 'utf8');
-//
-//    $('.highlight').each(function(index, element) {
-//      console.log('found highlight');
-//      var language = $(this).find('code').attr('class');
-//      var text = $(this).text();
-//      $(this).text('{% highlight js %}\n' + text + '\n{% endhighlight %}');
-//    });
-//
-//    $('.side-nav').append('<li><a href="#">' + post + '</a></li>');
-//    var html = marked(file, { renderer: renderer });
-//    $('#editor').html(html);
-//    var options = {
-//      editor: document.getElementById('editor')
-//    };
-//    var editor = new Pen(options);
-//  });
-//
-//});
-
-
 var Home = React.createClass({
 
   componentDidMount: function() {
@@ -205,10 +82,13 @@ var Home = React.createClass({
       if (postFile.match(postSlug)) {
         console.log(postFile);
         var file = fs.readFileSync(path.join(postsDir, postFile), 'utf8');
+        window.file = file;
+        window.markdown = markdown;
         file = file.split('---');
         file[2] = markdown;
         file.join('---');
-        fs.writeFileSync(path.join(postsDir, postFile), file);
+        console.log(file);
+//        fs.writeFileSync(path.join(postsDir, postFile), file);
         console.log('File saved');
       }
     });
